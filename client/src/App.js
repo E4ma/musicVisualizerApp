fce5c2ef0b1cdf81883649670dd568d5086c03d7
E4ma <ifeomaidoko@gmail.com>
2021-01-13 11:57:51 -0700

    client + server

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import VisualizerPage from './Containers/VisualizerPage';
+
+function App() {
+  return (
+    <div>
+      <VisualizerPage />
+     
+    </div>
+  );
+}
+
+export default App;
