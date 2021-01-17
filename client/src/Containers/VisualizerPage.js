7718d445d9a582807279857616ed5ba45f50d03f
Fernando A Cerda <fcerda@users.noreply.github.com>
2021-01-17 08:28:34 -0700

    Changes

--- client/src/Containers/VisualizerPage.js
+++ client/src/Containers/VisualizerPage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { createRef, useState, useEffect } from "react";
 import { Container, Grid, Button, Image } from "semantic-ui-react";
 import AudioComponent from "../Components/LeftColumnItems/Audio";
@@ -216,67 +215,5 @@ const VisualizerPage = (props) => {
     </Container>
   );
 };
-=======
-import React from 'react'
-import { Button, Card, ListGroup, Nav } from 'react-bootstrap'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Audio from '../Components/LeftColumnItems/Audio';
-import Background from '../Components/LeftColumnItems/Background';
-
-const VisualizerPage = () => {
-    return (
-
-        <Card style={{ width: '20rem' }}>
-            {/* <Card.Header>Featured</Card.Header> */}
-            <Router>
-                <ListGroup variant="flush">
-                    <Card.Body>
-
-                        <ListGroup.Item>
-                            <Nav.Link href="/Audio">
-                                <Button className="ui secondary button">Audio </Button>
-                            </Nav.Link>
-                        </ListGroup.Item>
-
-                        <ListGroup.Item>
-                            <Nav.Link href="/Background">
-                                <Button className="ui secondary button">Background </Button>
-                            </Nav.Link>
-                        </ListGroup.Item>
-
-                        <ListGroup.Item>
-                            <Button className="ui secondary button">Icon </Button>
-                        </ListGroup.Item>
-
-                        <ListGroup.Item>
-                            <Button className="ui secondary button">Text </Button>
-                        </ListGroup.Item>
-
-                        <ListGroup.Item>
-                            <Button className="ui secondary button">Export </Button>
-                            <Button className="ui secondary button">Save </Button>
-                        </ListGroup.Item>
-
-                    </Card.Body>
-                </ListGroup>
-
-
-                <Switch>
-                    <Route path="/Audio" exact component={Audio} />
-                    {/* <Audio />
-                    </Route> */}
-                    <Route path="/Background">
-                        <Background />
-                    </Route>
-                </Switch>
-
-            </Router>
-
-
-        </Card>
-
-    )
-}
->>>>>>> 988dc2454f20bc0fc0fee5db9c685884bf903371
 
 export default VisualizerPage;
