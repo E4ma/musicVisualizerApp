12c2230bdf45e6f0152220d8f5d5f8f776513353
Kurt Ball <kurtisdball@gmail.com>
2021-01-27 18:21:13 -0700

    Server and .env

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 //Import Routes
 const express = require('express')
 //const fileUpload = require('express-fileupload')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const app = express()
@@ -8,6 +9,15 @@ const mediaSort = require('./Routes/mediaSort')
 //Needed for the process.env code
 dotenv.config()
 //Used to keep private information hidden
+
+//Connecting to the db
+mongoose
+  .connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Could not connect to MongoDB...', err))
 
 //Middleware
 app.use(cors())
