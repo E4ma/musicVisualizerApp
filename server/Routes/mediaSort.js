def8cc3a7e9f2d024881df1359f5524f0aad785d
Kurt Ball <kurtisdball@hotmail.com>
2021-02-06 13:38:04 -0700

    Incorporated connected to Mediasort

--- server/Routes/mediaSort.js
+++ server/Routes/mediaSort.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const fileUpload = require('express-fileupload')
+const fs = require('fs')
+const path = require('path')
 
 //enable the file upload
 router.use(fileUpload())
@@ -29,12 +31,13 @@ router.post('/icon', (req, res) => {
 
 //Upload endpoints for music and audio not related to the icon
 router.post('/media', async (req, res) => {
+  // console.log(req.files)
   if (req.files === null) {
     return res.status(400).json({ msg: 'no file found' })
   }
   //will define what file is in React
   const file = req.files.file
-  console.log(file)
+  // console.log(file)
   if (file.size > fileSizeMax) {
     return res.status(413).json({ msg: 'File exceeds upload size' })
   }
@@ -45,12 +48,20 @@ router.post('/media', async (req, res) => {
 
   if (mediaType == 'audio/mpeg' || 'audio/mp3' || 'audio/flac' || 'audio/wav') {
     file.mv(`uploadedFiles/audio/${file.name}`)
-
+    console.log('Line 50')
     const filePath = `uploadedFiles/audio/${file.name}`
-
+    console.log('Line 52', filePath)
+    // const metadata = await mm.parseFile(`${filePath}`)
+    // console.log("Line 54" )
+    // const parsedMetaData = util.inspect(metadata, {
+    //   showHidden: false,
+    //   depth: null,
+    // })
+    // console.log(`${parsedMetaData}`)
     res.json({
       fileName: file.name,
       filePath: filePath,
+      // metaData: parsedMetaData,
     })
   } else if (mediaType == 'image/jpeg' || 'image/jpg' || 'image/bmp') {
     file.mv(`uploadedFiles/images/${file.name}`)
@@ -62,6 +73,24 @@ router.post('/media', async (req, res) => {
   } else {
     return res.status(500).send(console.error('Internal Server Error X|'))
   }
+})
+
+//export to displayer
+router.get('/list', async (req, res) => {
+  console.log('this is the req on line 80')
+  fs.readdir('./uploadedFiles/audio', (err, files) => {
+    if (err) console.log(err, 'Some error after line 81')
+    else {
+      files.forEach((file) => {
+        console.log(file, 'line 85')
+      })
+      res.json(files)
+    }
+  })
+})
+router.get('/media/:song', (req, res) => {
+  const song = req.params.song
+  res.sendFile(path.join(__dirname, `../uploadedFiles/audio/${song}`))
 })
 
 module.exports = router
