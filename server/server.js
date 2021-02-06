9f6cac678f3f6a40a0561bf1a06f75ab7a8100a0
Kurt Ball <kurtisdball@hotmail.com>
2021-02-06 13:40:09 -0700

    Server connecting Fernando changes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 //Import Routes
 const express = require('express')
-//const fileUpload = require('express-fileupload')
+// const fileuploader = require('express-fileupload')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const cors = require('cors')
-const app = express()
+// const app = express()
 const mediaSort = require('./Routes/mediaSort')
-// const audioDemo = require('./Routes/sendMedia')
-const userRoutes = require('./Routes/userRoutes')
+//const download = require('./Routes/download')
+
+//Models
+require('./UserModel')
+const userRoutes = require('./routes')
+
+const app = express()
+// A middleware which will enable cors with various options like exposeHeader etc.
+app.use(cors({ exposeHeader: 'id' }))
+// A middleware which will extract req.body for us
+app.use(express.json())
+// A middleware which will extract re.files for us
+// app.use(fileuploader());
+
+// Routes
+app.use('/user', userRoutes)
 
 //Needed for the process.env code
 dotenv.config()
@@ -22,12 +36,24 @@ mongoose
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Could not connect to MongoDB...', err))
 
+//Connecting to the db
+// mongoose
+//   .connect('mongodb://localhost/UploadUserInfo', {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//   })
+//   .then(() => {
+//     console.log('We are connected to MongoDB')
+//   })
+//   .catch((err) => {
+//     console.log(err.message)
+//   })
+
 //Middleware
-app.use(express.json())
 app.use(cors())
 app.use('/upload', mediaSort)
-app.use('/user', userRoutes)
-// app.use('/audioDemo', audioDemo)
+//app.use('/download', download)
 
 port = process.env.PORT
 
