49ab388c035f15d60f37b237ae2527f91a00c0f0
Kurt Ball <kurtisdball@hotmail.com>
2021-02-06 15:37:13 -0700

    Updated server and cleaned up

--- server/server.js
+++ server/server.js
@@ -1,27 +1,26 @@
 //Import Routes
 const express = require('express')
-// const fileuploader = require('express-fileupload')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const cors = require('cors')
-// const app = express()
 const mediaSort = require('./Routes/mediaSort')
 //const download = require('./Routes/download')
 
 //Models
-require('./UserModel')
-const userRoutes = require('./routes')
+// const UserModel = require('./UserModel')
+const userRoutes = require('./Routes/userRoutes')
 
 const app = express()
-// A middleware which will enable cors with various options like exposeHeader etc.
-app.use(cors({ exposeHeader: 'id' }))
+
 // A middleware which will extract req.body for us
 app.use(express.json())
-// A middleware which will extract re.files for us
-// app.use(fileuploader());
 
 // Routes
 app.use('/user', userRoutes)
+
+//Middleware
+app.use(cors())
+app.use('/upload', mediaSort)
 
 //Needed for the process.env code
 dotenv.config()
@@ -35,25 +34,6 @@ mongoose
   })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Could not connect to MongoDB...', err))
-
-//Connecting to the db
-// mongoose
-//   .connect('mongodb://localhost/UploadUserInfo', {
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//   })
-//   .then(() => {
-//     console.log('We are connected to MongoDB')
-//   })
-//   .catch((err) => {
-//     console.log(err.message)
-//   })
-
-//Middleware
-app.use(cors())
-app.use('/upload', mediaSort)
-//app.use('/download', download)
 
 port = process.env.PORT
 
