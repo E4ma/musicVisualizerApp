497e0442dc2d916220393a76aae2d2c6c2e4cbae
Kurt Ball <kurtisdball@hotmail.com>
2021-02-17 16:15:02 -0700

    Creating components

--- client/src/Components/MediaInteractions/Playlist.js
+++ client/src/Components/MediaInteractions/Playlist.js
@@ -1,17 +1,47 @@
-import React, { useContext, useEffect } from 'react'
-import { PlaylistContext } from '../../contexts/PlaylistContext'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Card } from 'react-bootstrap'
 
+let audio
+let audioContext
+let source
+let analyser
+let frequency_array
+const createAudioContext = () => {
+  audio = new Audio()
+
+  audioContext = new (window.AudioContext || window.webkitAudioContext)()
+  source = audioContext.createMediaElementSource(audio)
+  analyser = audioContext.createAnalyser()
+  source.connect(analyser)
+  analyser.connect(audioContext.destination)
+  frequency_array = new Uint8Array(analyser.frequencyBinCount)
+}
+
 const Playlist = (props) => {
-  const { songSelect, setsongSelect, getSong } = useContext(PlaylistContext)
+  const [playlist, setPlaylist] = useState([])
+
+  const getSong = async (song) => {
+    console.log('This is in getSong', song)
+    createAudioContext()
+    const response = await axios.request({
+      url: `http://localhost:5000/upload/media/${song}`,
+      responseType: 'blob',
+      method: 'GET',
+    })
+    audio.src = URL.createObjectURL(response.data)
+    // console.log('audio.src', audio.src)
+    audio.load()
+    // audio.play()
+  }
+
   const getSongList = async () => {
     let res = await axios.get('http://localhost:5000/upload/list')
     const data = res.data
     console.log('This is data', data)
 
-    setsongSelect(data)
-    console.log('This is the songSelect', songSelect)
+    setPlaylist(data)
+    console.log('This is the playlist', playlist)
   }
   // console.log(uploadedFile)
   //Returns a list of files saved
@@ -30,8 +60,8 @@ const Playlist = (props) => {
                 getSong(event.target.firstChild.data)
               }}
             >
-              {songSelect?.map((song, index) => {
-                // console.log('This is the song with index', song, index)
+              {playlist?.map((song, index) => {
+                console.log('This is the song with index', song, index)
                 return (
                   <li key={index} value={song}>
                     {song}
