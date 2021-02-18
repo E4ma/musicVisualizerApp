7e1c5c9b192ddadeb8127e20857146fb689da987
Samsudeen Naina <samsudeen54@gmail.com>
2021-02-18 00:43:41 -0700

    upload_modal_half_done

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Card, Row, Col, Nav } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Card, Row, Col, Nav, Modal } from 'react-bootstrap'
 import Tab from 'react-bootstrap/Tab'
 import Displayer from './Displayer'
 import FileUpload from './MediaInteractions/FileUpload'
@@ -7,112 +7,144 @@ import InsertIcon from './MediaInteractions/InsertIcon'
 import Navigation from './Navigation'
 // import Playlist from './MediaInteractions/Playlist'
 
+
 const Home = () => {
 
+  // This is for controlling the Modal window (AUDIO)
+  const [isOpen, setIsOpen] = useState(false);
+  const showModal = () => {
+    setIsOpen(true)
+  };
+  const hideModal = () => {
+    setIsOpen(false)
+  };
 
-   return (
-      <div>
-         <Navigation />
-         <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-            <Row>
-               <Col md={2}>
-                  <Nav
-                     variant="pills"
-                     className="flex-column textbold"
-                     bg="transparent"
-                     style={{ fontWeight: 'bold' }}
-                  >
-                     <br />
+  // This is for controlling the Modal window (BACKGROUND)
+  const [BackModelOpen, setBackModelOpen] = useState(false);
+  const showBackModal = () => {
+    setBackModelOpen(true)
+  };
+  const hideBackModal = () => {
+    setBackModelOpen(false)
+  };
 
-                     {/*------ Audio -----*/}
-                     <Nav.Item>
-                        <Nav.Link
-                           eventKey="uploadAudio"
-                           style={{
-                              borderRadius: '22px',
-                              backgroundColor: 'transparent',
-                           }}
-                        >
-                           <button className="btn1">Audio</button>
-                        </Nav.Link>
-                     </Nav.Item>
-                     <br />
-                     <br />
 
-                     {/*------ Background -----*/}
-                     <Nav.Item>
-                        <Nav.Link
-                           eventKey="uploadBackground"
-                           style={{
-                              borderRadius: '22px',
-                              backgroundColor: 'transparent',
-                           }}
-                        >
-                           <button className="btn1">Background</button>
-                        </Nav.Link>
-                     </Nav.Item>
-                     <br />
-                     <br />
+  return (
 
-                     {/*------ Icon -----*/}
-                     <Nav.Item>
-                        <Nav.Link
-                           eventKey="uploadIcon"
-                           style={{
-                              borderRadius: '22px',
-                              backgroundColor: 'transparent',
-                           }}
-                        >
-                           <button className="btn1">Icon</button>
-                        </Nav.Link>
-                     </Nav.Item>
-                     <br />
-                     <br />
+    <div>
+      <Navigation />
+      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+        <Row>
+          <Col md={2}>
+            <Nav
+              variant="pills"
+              className="flex-column textbold"
+              bg="transparent"
+              style={{ fontWeight: 'bold' }}
+            >
+              <br />
 
-                     {/*------ Text -----*/}
-                     <Nav.Item>
-                        <Nav.Link
-                           eventKey="home"
-                           style={{
-                              borderRadius: '22px',
-                              backgroundColor: 'transparent',
-                           }}
-                        >
-                           <button className="btn1">Visualizer</button>
-                        </Nav.Link>
-                     </Nav.Item>
-                  </Nav>
-               </Col>
+              {/*------ Audio -----*/}
 
-               <Col md={'auto'}>
-                  <Tab.Content>
-                     <Tab.Pane eventKey="uploadAudio" mediatype="audio">
-                        <FileUpload mediatype="Audio" />
-                     </Tab.Pane>
-                     <Tab.Pane eventKey="uploadBackground">
-                        <FileUpload mediatype="Background" filetype="image" />
-                     </Tab.Pane>
-                     <Tab.Pane eventKey="uploadIcon">
-                        <FileUpload mediatype="Icon" filetype="image" />
-                     </Tab.Pane>
-                  </Tab.Content>
-               </Col>
+              <Nav.Item>
+                <Nav.Link
+                  eventKey="uploadAudio"
+                  style={{
+                    borderRadius: '22px',
+                    backgroundColor: 'transparent',
+                  }}
+                >
+                  <button className="btn1" onClick={showModal}  >Audio</button>
+                </Nav.Link>
+              </Nav.Item>
 
-               {/* card for displaying the visualizer on the right panel*/}
 
-               <Col md xl={'auto'}>
-                  <br />
-                  <Card className="visualizer">
-                     <Card.Body>
-                        <Displayer />
-                        <InsertIcon />
-                     </Card.Body>
-                  </Card>
-               </Col>
-            </Row>
-         </Tab.Container>
-      </div>
-   )
+              <br />
+              <br />
+
+              {/*------ Background -----*/}
+              <Nav.Item>
+                <Nav.Link
+                  eventKey="uploadBackground"
+                  style={{
+                    borderRadius: '22px',
+                    backgroundColor: 'transparent',
+                  }}
+                >
+                  <button className="btn1" onClick={showBackModal} >Background</button>
+                </Nav.Link>
+              </Nav.Item>
+              <br />
+              <br />
+
+              {/*------ Icon -----*/}
+              <Nav.Item>
+                <Nav.Link
+                  eventKey="uploadIcon"
+                  style={{
+                    borderRadius: '22px',
+                    backgroundColor: 'transparent',
+                  }}
+                >
+                  <button className="btn1">Icon</button>
+                </Nav.Link>
+              </Nav.Item>
+              <br />
+              <br />
+
+              {/*------ Text -----*/}
+              <Nav.Item>
+                <Nav.Link
+                  eventKey="home"
+                  style={{
+                    borderRadius: '22px',
+                    backgroundColor: 'transparent',
+                  }}
+                >
+                  <button className="btn1">Visualizer</button>
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+
+          <Col md={'auto'}>
+            <Tab.Content>
+
+              <Tab.Pane eventKey="uploadAudio" mediatype="audio">
+                <Modal className="modalUpload" show={isOpen} onHide={hideModal}>
+                  <FileUpload mediatype="Audio" />
+                  <button className="btn2" onClick={hideModal} > Cancel</button>
+                </Modal>
+              </Tab.Pane>
+
+              <Tab.Pane eventKey="uploadBackground" mediatype="image">
+                <Modal show={BackModelOpen} onHide={hideBackModal}>
+                  <FileUpload mediatype="Background" filetype="image" />
+                  <button className="btn2" onClick={hideBackModal} > Cancel</button>
+                </Modal>
+              </Tab.Pane>
+
+              <Tab.Pane eventKey="uploadIcon">
+                <FileUpload mediatype="Icon" filetype="image" />
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+
+          {/* card for displaying the visualizer on the right panel*/}
+
+          <Col md xl={'auto'}>
+            <br />
+            <Card className="visualizer">
+              <Card.Body>
+                <Displayer />
+                <InsertIcon />
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Tab.Container>
+    </div>
+  )
 }
 
 export default Home
--- client/src/Components/MediaInteractions/FileUpload.js
+++ client/src/Components/MediaInteractions/FileUpload.js
@@ -4,189 +4,194 @@ import { Card } from 'react-bootstrap'
 import Playlist from './Playlist'
 
 const FileUpload = (props) => {
-	//Need to use a hook to set text in the label to the file namespace
-	const [file, setFile] = useState('')
-	const [filename, setFilename] = useState('Choose File')
-	const [uploadedFile, setUploadedFile] = useState({})
-	const [playlist, setPlaylist] = useState([])
+  //Need to use a hook to set text in the label to the file namespace
+  const [file, setFile] = useState('')
+  const [filename, setFilename] = useState('Choose File')
+  const [uploadedFile, setUploadedFile] = useState({})
+  const [playlist, setPlaylist] = useState([])
 
-	const onChange = (e) => {
-		//HTML file uploads come as an array so we want the index of the first file
-		setFile(e.target.files[0])
-		setFilename(e.target.files[0].name)
-	}
+  const onChange = (e) => {
+    //HTML file uploads come as an array so we want the index of the first file
+    setFile(e.target.files[0])
+    setFilename(e.target.files[0].name)
+  }
 
-	const onSubmit = async (e) => {
-		// e.preventDefault() used to prevent submitting by accident by preventing normal submitting
-		e.preventDefault()
+  const onSubmit = async (e) => {
+    // e.preventDefault() used to prevent submitting by accident by preventing normal submitting
+    e.preventDefault()
 
-		const formData = new FormData()
-		formData.append('file', file)
+    const formData = new FormData()
+    formData.append('file', file)
 
-		try {
-			const res = await axios.post(
-				'http://localhost:5000/upload/media',
-				formData,
-				{
-					headers: { 'Content-Type': 'multipart/form-data' },
-				},
-			)
-			const { fileName, filePath } = res.data
-			// console.log('These are the headers', res.headers)
-			if (res.status === 200) {
-				console.log('File was uploaded successfully ' + res.status)
-			}
-			setUploadedFile({ fileName, filePath })
-		} catch (err) {
-			if (err.response.status === 500) {
-				console.error('There is a problem with the server ' + err.message)
-			} else {
-				//This message is from the server if no file is uploaded
-				// console.error('we had an error??? ' + err.response.data.msg)
-				console.error(err.response.data.msg)
-			}
-		}
-	}
+    try {
+      const res = await axios.post(
+        'http://localhost:5000/upload/media',
+        formData,
+        {
+          headers: { 'Content-Type': 'multipart/form-data' },
+        },
+      )
+      const { fileName, filePath } = res.data
+      // console.log('These are the headers', res.headers)
+      if (res.status === 200) {
+        console.log('File was uploaded successfully ' + res.status)
+      }
+      setUploadedFile({ fileName, filePath })
+    } catch (err) {
+      if (err.response.status === 500) {
+        console.error('There is a problem with the server ' + err.message)
+      } else {
+        //This message is from the server if no file is uploaded
+        // console.error('we had an error??? ' + err.response.data.msg)
+        console.error(err.response.data.msg)
+      }
+    }
+  }
 
 
-	// Gets the playlist and sends to the Playlist Component
-	const getPlaylist = () => {
-		axios
-			.get('http://localhost:5000/upload/list')
-			.then((res) => {
-				// console.log('this is the res', res)
-				return res.data
-			})
-			.then((playlist) => {
-				// console.log('This is the playlist', playlist)
-				setPlaylist(playlist)
-			})
-	}
-	useEffect(() => {
-		getPlaylist()
-	}, [uploadedFile])
+  // Gets the playlist and sends to the Playlist Component
+  const getPlaylist = () => {
+    axios
+      .get('http://localhost:5000/upload/list')
+      .then((res) => {
+        // console.log('this is the res', res)
+        return res.data
+      })
+      .then((playlist) => {
+        // console.log('This is the playlist', playlist)
+        setPlaylist(playlist)
+      })
+  }
+  useEffect(() => {
+    getPlaylist()
+  }, [uploadedFile])
 
-	// If the audio button is selected
-	if (props.mediatype === 'Audio') {
-		return (
-			<Card style={{ width: '16rem', margin: '16px' }}>
-				<Card.Body>
-					<Card.Title>{props.mediatype} Upload</Card.Title>
-					<Card.Subtitle className="mb-2 text-muted">
-						Select {props.mediatype} to Upload
+  // If the Audio button is selected
+  if (props.mediatype === 'Audio') {
+    return (
+      // <Card style={{ width: '29rem', margin: '16px' }}>
+      <Card >
+        <Card.Body className="modalUpload">
+          <Card.Title>{props.mediatype} Upload</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            Select {props.mediatype} to Upload
           </Card.Subtitle>
-					<Card.Text>
-						<>
-							<div className="mb-2">
-								<form onSubmit={onSubmit}>
-									<div className="input-group mb-3">
-										<input
-											type="file"
-											className="form-control mb-5"
-											id="inputGroupFile02"
-											accept={`${props.mediatype}/wav, ${props.mediatype}/mp3, ${props.mediatype}/mpeg`}
-											onChange={onChange}
-										/>
-										<input
-											type="submit"
-											value={`Submit`}
-											// className="btn btn-primary btn-block"
-											className="btn1"
-											onClick={() => getPlaylist()}
-										/>
+          <Card.Text>
+            <>
+              <div className="mb-2">
+                <form onSubmit={onSubmit}>
+                  {/* <div className="input-group mb-4"> */}
+                  <div >
+                    <input
+                      type="file"
+                      // className="form-control mb-5"
+                      className="modalUpload"
+                      id="inputGroupFile02"
+                      accept={`${props.mediatype}/wav, ${props.mediatype}/mp3, ${props.mediatype}/mpeg`}
+                      onChange={onChange}
+                    />
+                    <input
+                      type="submit"
+                      value={`Submit`}
+                      // className="btn btn-primary btn-block"
+                      className="btn2"
+                      onClick={() => getPlaylist()}
+                    />
 
-										<label
-											className="id=inputGroupFile02"
-											htmlFor="inputGroupFile02"
-										></label>
-									</div>
-								</form>
-							</div>
-						</>
-					</Card.Text>
-				</Card.Body>
-				<Playlist uploadedFile={uploadedFile} />
-			</Card>
-		)
-	}
-	if (props.mediatype === 'Background') {
-		return (
-			<Card style={{ width: '16rem', margin: '16px' }}>
-				<Card.Body>
-					<Card.Title>{props.mediatype} Upload</Card.Title>
-					<Card.Subtitle className="mb-2 text-muted">
-						Select {props.mediatype} to Upload
+                    <label
+                      className="id=inputGroupFile02"
+                      htmlFor="inputGroupFile02"
+                    ></label>
+                  </div>
+                </form>
+              </div>
+            </>
+          </Card.Text>
+        </Card.Body>
+        <Playlist uploadedFile={uploadedFile} />
+      </Card>
+    )
+  }
+  if (props.mediatype === 'Background') {
+    return (
+      <Card >
+        <Card.Body className="modalUpload">
+          <Card.Title>{props.mediatype} Upload</Card.Title>
+          <Card.Subtitle  className="mb-2 text-muted">
+            Select {props.mediatype} to Upload
           </Card.Subtitle>
-					<Card.Text>
-						<>
-							<div className="mb-2">
-								<form onSubmit={onSubmit}>
-									<div className="input-group mb-3">
-										<input
-											type="file"
-											className="form-control mb-5"
-											id="inputGroupFile02"
-											accept={`${props.filetype}/jpg, ${props.filetype}/jpeg, ${props.filetype}/bmp`}
-											onChange={onChange}
-										/>
-										<input
-											type="submit"
-											value={`Submit`}
-											// className="btn btn-primary btn-block"
-											className="btn1"
-										/>
+          <Card.Text>
+            <>
+              <div className="mb-2">
+                <form onSubmit={onSubmit}>
+                  {/* <div className="input-group mb-3"> */}
+                  <div>
+                    <input
+                      type="file"
+                      // className="form-control mb-5"
+                      className="modalUpload"
+                      id="inputGroupFile02"
+                      accept={`${props.filetype}/jpg, ${props.filetype}/jpeg, ${props.filetype}/bmp`}
+                      onChange={onChange}
+                    />
+                    <input
+                      type="submit"
+                      value={`Submit`}
+                      // className="btn btn-primary btn-block"
+                      className="btn2"
+                    />
 
-										<label
-											className="id=inputGroupFile02"
-											htmlFor="inputGroupFile02"
-										></label>
-									</div>
-								</form>
-							</div>
-						</>
-					</Card.Text>
-				</Card.Body>
-			</Card>
-		)
-	}
-	return (
-		<Card style={{ width: '16rem', margin: '16px' }}>
-			<Card.Body>
-				<Card.Title>{props.mediatype} Upload</Card.Title>
-				<Card.Subtitle className="mb-2 text-muted">
-					Select {props.mediatype} to Upload
+                    <label
+                      className="id=inputGroupFile02"
+                      htmlFor="inputGroupFile02"
+                    ></label>
+                  </div>
+                </form>
+              </div>
+            </>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    )
+  }
+  return (
+    <Card style={{ width: '16rem', margin: '16px' }}>
+      <Card.Body>
+        <Card.Title>{props.mediatype} Upload</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">
+          Select {props.mediatype} to Upload
         </Card.Subtitle>
-				<Card.Text>
-					<>
-						<div className="mb-2">
-							<form onSubmit={onSubmit}>
-								<div className="input-group mb-3">
-									<input
-										type="file"
-										className="form-control mb-5"
-										id="inputGroupFile02"
-										accept={`${props.filetype}/gif, ${props.filetype}/png, ${props.filetype}/jpg`}
-										onChange={onChange}
-									/>
-									<input
-										type="submit"
-										value={`Submit`}
-										// className="btn btn-primary btn-block"
-										className="btn1"
-									/>
+        <Card.Text>
+          <>
+            <div className="mb-2">
+              <form onSubmit={onSubmit}>
+                <div className="input-group mb-3">
+                  <input
+                    type="file"
+                    className="form-control mb-5"
+                    id="inputGroupFile02"
+                    accept={`${props.filetype}/gif, ${props.filetype}/png, ${props.filetype}/jpg`}
+                    onChange={onChange}
+                  />
+                  <input
+                    type="submit"
+                    value={`Submit`}
+                    // className="btn btn-primary btn-block"
+                    className="btn1"
+                  />
 
-									<label
-										className="id=inputGroupFile02"
-										htmlFor="inputGroupFile02"
-									></label>
-								</div>
-							</form>
-						</div>
-					</>
-				</Card.Text>
-			</Card.Body>
-		</Card>
-	)
+                  <label
+                    className="id=inputGroupFile02"
+                    htmlFor="inputGroupFile02"
+                  ></label>
+                </div>
+              </form>
+            </div>
+          </>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  )
 }
 
 export default FileUpload
